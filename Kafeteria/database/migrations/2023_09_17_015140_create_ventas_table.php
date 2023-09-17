1cdc7dac2e03a261663b92a619bd877315791af6
Miguel David Castaño Salgado <110947642+miguedavidcs@users.noreply.github.com>
2023-09-16 20:56:13 -0500

    Creamos las Tablas Entidades o Migraciones

---
+++ Kafeteria/database/migrations/2023_09_17_015140_create_ventas_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ventas', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_producto');
+            $table->date('fecha_venta');
+            $table->integer('cantidad_vendida');
+            $table->decimal('monto_total', 10, 2);
+            $table->timestamps();
+            // Esta es la relacion
+            $table->foreign('id_producto')->references('id')->on('productos')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ventas');
+    }
+};
